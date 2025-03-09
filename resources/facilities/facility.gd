b58d0547166dabe2d2182fe6be30aeffcc159e9a
Dylan Bulfin <dylanbulfin@gmail.com>
2025-03-09 19:59:53 -0400

    Updating hundreds multiplier

--- resources/facilities/facility.gd
+++ resources/facilities/facility.gd
@@ -6,4 +6,4 @@ class_name FacilityResource
 @export var base_output: float
 @export var cost_ratio: float = 1.35
 @export var tens_multiplier: float = 4
-@export var hundreds_multiplier: float = 100
+@export var hundreds_multiplier: float = 50
